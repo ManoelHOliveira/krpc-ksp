cf0f83cf3e455c957599670be3dd31206b86e4a6
ManoelHOliveira <manoelhenriquedeoliveirabr@gmail.com>
2026-05-01 13:12:44 -0300

    adiciona type hints

--- ascent.py
+++ ascent.py
@@ -1,16 +1,14 @@
-import krpc
 import time
-import subprocess
-import os
 from connection import get_connection
+from krpc.services.spacecenter import Vessel
 
-def stage(vessel):
+def stage(vessel: Vessel):
     vessel.control.activate_next_stage()
 
-def set_throttle_max(vessel):
+def set_throttle_max(vessel: Vessel):
     vessel.control.throttle = 1.
 
-def set_throttle_min(vessel):
+def set_throttle_min(vessel: Vessel):
     vessel.control.throttle = 0.
 
 def countdown(seconds):
@@ -19,16 +17,16 @@ def countdown(seconds):
         print(i)
 
 
-def do_launch(vessel):
+def do_launch(vessel: Vessel):
     print(f"Ligando motor por 5 segundos")
 
     # testa o motor principal do foguete por alguns segundos
     stage(vessel)
-    
+
     set_throttle_max(vessel)
     countdown(5)
     set_throttle_min(vessel)
-    
+
 
 def main():
     conn = get_connection("Ascent")
